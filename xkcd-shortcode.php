fa3c735b7f576f2afd53e511fff0340093bbaddd
Darshan Sawardekar <darshan@sawardekar.org>
2014-02-13 16:31:18 +0530

    Adds scaffolding.

---
+++ xkcd-shortcode.php
@@ -0,0 +1,11 @@
+<?php
+/*
+* Plugin Name: xkcd-shortcode
+* Description: Provides an [xkcd] shortcode.
+* Version: 0.1
+* Author: Darshan Sawardekar
+* Author URI: http://pressing-matters.io/
+* Plugin URI: http://wordpress.org/plugins/xkcd-shortcode
+**/
+
+?>
