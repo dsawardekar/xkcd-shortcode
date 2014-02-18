0cca2e0afa85a8514e7d2a508d76a5f41c22c2aa
Darshan Sawardekar <darshan@sawardekar.org>
2014-02-19 00:10:51 +0530

    Fixes meta-info.

--- xkcd-shortcode.php
+++ xkcd-shortcode.php
@@ -1,11 +1,12 @@
 <?php
 /*
 * Plugin Name: xkcd-shortcode
-* Description: Provides an [xkcd] shortcode that loads the XKCD webcomic by number.
+* Description: Adds an [xkcd] shortcode that loads the XKCD webcomic by number.
 * Version: 0.1
 * Author: Darshan Sawardekar
 * Author URI: http://pressing-matters.io/
 * Plugin URI: http://wordpress.org/plugins/xkcd-shortcode
+* License: GPLv2
 **/
 
 require_once(__DIR__ . '/lib/XKCDShortcodePlugin.php');
