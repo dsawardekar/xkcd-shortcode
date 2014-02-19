6a2b6d29f6a32c9425145b2940c4576276905edd
Darshan Sawardekar <darshan@sawardekar.org>
2014-02-19 13:45:19 +0530

    Testing 0.2

--- xkcd-shortcode.php
+++ xkcd-shortcode.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: xkcd-shortcode
 Description: Adds an [xkcd] shortcode that loads the XKCD webcomic by number.
-Version: 0.1
+Version: 0.2
 Author: Darshan Sawardekar
 Author URI: http://pressing-matters.io/
 Plugin URI: http://wordpress.org/plugins/xkcd-shortcode
