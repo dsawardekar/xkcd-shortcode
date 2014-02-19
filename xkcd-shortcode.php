41e2d72ec6910893eadd138c87423a636aeb6ef1
Darshan Sawardekar <darshan@sawardekar.org>
2014-02-19 13:35:39 +0530

    Header cleanup

--- xkcd-shortcode.php
+++ xkcd-shortcode.php
@@ -1,13 +1,13 @@
 <?php
 /*
-* Plugin Name: xkcd-shortcode
-* Description: Adds an [xkcd] shortcode that loads the XKCD webcomic by number.
-* Version: 0.1
-* Author: Darshan Sawardekar
-* Author URI: http://pressing-matters.io/
-* Plugin URI: http://wordpress.org/plugins/xkcd-shortcode
-* License: GPLv2
-**/
+Plugin Name: xkcd-shortcode
+Description: Adds an [xkcd] shortcode that loads the XKCD webcomic by number.
+Version: 0.1
+Author: Darshan Sawardekar
+Author URI: http://pressing-matters.io/
+Plugin URI: http://wordpress.org/plugins/xkcd-shortcode
+License: GPLv2
+*/
 
 require_once(__DIR__ . '/lib/XKCDShortcodePlugin.php');
 
